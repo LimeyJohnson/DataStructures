7a99d6ed7bbdd3040e64b7a8d2d0aabc1891d8c1
Andrew Johnson <LimeyJohnson@gmail.com>
2016-04-03 20:00:24 -0700

    Initial Commit

---
+++ DataStructures/BinarySearchTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class BinarySearchTree<T> where T :struct, IComparable
+    {
+       Node<T> Root { get; set; }
+        public void Add(T value)
+        {
+            var node = new Node<T>(value);
+            if (Root == null)
+            {
+                Root = node;
+                return;
+            }
+            var parent = Root;
+            var found = false;
+            while(!found)
+            {
+                var result = parent.Value.CompareTo(value);
+                if(result > 0)
+                {
+                    if(parent.Right == null)
+                    {
+                        parent.Right = node;
+                        found = true;
+                    }
+                    else
+                    {
+                        parent = parent.Right;
+                    }
+                }
+                if (result < 0)
+                {
+                    if (parent.Left == null)
+                    {
+                        parent.Left = node;
+                        found = true;
+                    }
+                    else
+                    {
+                        parent = parent.Left;
+                    }
+                }
+                if (result == 0) throw new ArgumentOutOfRangeException("value", "Duplicate Record Detected");
+            }
+        }
+    }
+    
+    
+}
---
+++ DataStructures/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Node<T> where T :struct
+    {
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+        public T Value { get; set; }
+        public Node (T value)
+        {
+            this.Value = value;
+        }
+    }
+}
